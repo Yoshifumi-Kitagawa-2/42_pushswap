7484af87dd108b1c92c56d0eb2c7a8b340e66aef
Yoshifumi-Kitagawa-2 <kitapo3110@gmail.com>
2023-01-17 11:40:00 +0900

    commit

--- srcs/push_swap.c
+++ srcs/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: yokitaga <yokitaga@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/07 23:28:10 by yokitaga          #+#    #+#             */
-/*   Updated: 2023/01/17 00:40:22 by yokitaga         ###   ########.fr       */
+/*   Updated: 2023/01/17 11:38:30 by yokitaga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,10 +28,11 @@ t_node *init_node(int data)
     return (node);
 }
 
+/*
 t_list *init_list(int data)
 {
     t_list *list;
-
+    
     list = NULL;
     list = (t_list *)malloc(sizeof(t_list));
     if (list == NULL)
@@ -48,16 +49,46 @@ t_list *init_list(int data)
     }
     return (list);
 }
+*/
 
-
-
-int main(int argc, char **argv)
+t_list *init_list(int argc, char **argv)
 {
-    t_node *node;
+    t_list *list;
+    size_t i;
+    int    array[argc - 1];
+    
+    list = NULL;
+    list = (t_list *)malloc(sizeof(t_list));
+    if (list == NULL)
+    {
+        printf("init_list malloc error\n");
+        return (NULL);
+    }
 
-    node = (t_node *)malloc(sizeof(t_node));
-    node->data = 0;
-    printf("%d\n", node->data);
-    free(node);
+    i = 0;
+    while (argv[i + 1] != NULL)
+    {
+        array[i] = ft_atoi(argv[i + 1]);
+        
+        i++;
+    }
+    list->list_top_node = init_node(data);
+    if (list->list_top_node == NULL)
+    {
+        printf("init_node malloc error\n");
+        free(list);
+        return (NULL);
+    }
+    return (list);
+}
+
+
+
+int main(int argc, char *argv[])
+{
+    t_list *list;
+
+    list = init_list(argc, argv);
+
     return(0);
 }
