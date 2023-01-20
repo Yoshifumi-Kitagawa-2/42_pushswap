bcaf839fe481d07f10965d4598b661b49b19b1ff
Yoshifumi-Kitagawa-2 <kitapo3110@gmail.com>
2023-01-20 13:48:35 +0900

    commit

--- srcs/push_swap.h
+++ srcs/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: yokitaga <yokitaga@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/07 23:30:31 by yokitaga          #+#    #+#             */
-/*   Updated: 2023/01/19 21:36:33 by yokitaga         ###   ########.fr       */
+/*   Updated: 2023/01/20 13:48:10 by yokitaga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,18 +38,28 @@ typedef struct s_data{
     size_t stack_len;
 }t_data;
 
-t_node *create_stack_b();
-t_node *create_node(int value);
-t_node *create_stack(int data[],int n);
+t_node  *create_stack_b();
+t_node  *create_node(int value);
+t_node  *create_stack(int data[],int n);
 
-size_t ft_stack_size(t_node *stack);
-void   ft_stack_last(t_node *stack);
-void ft_stack_add_front(t_node **stack, t_node *new);
+size_t  ft_stack_size(t_node *stack);
+void    ft_stack_last(t_node *stack);
+void    ft_stack_add_front(t_node **stack, t_node *new);
 
-void   push_swap_sa(t_node **stack);
-void   push_swap_ra(t_node **stack);
-void   push_swap_rra(t_node **stack);
-t_node *push_swap_pop(t_node **stack_head);
-void   push_swap_pb(t_data *data);
+void    sa_or_sb(t_node **stack_head);
+void    operation_sa(t_data *data);
+void    operation_sb(t_data *data);
+
+void    ra_or_rb(t_node **stack_head);
+void    operation_ra(t_data *data);
+void    operation_rb(t_data *data);
+
+void    rra_or_rrb(t_node **stack_head);
+void    operation_rra(t_data *data);
+void    operation_rrb(t_data *data);
+
+t_node  *pop(t_node **stack_head);
+void    operation_pa(t_data *data);
+void    operation_pb(t_data *data);
 
 #endif
