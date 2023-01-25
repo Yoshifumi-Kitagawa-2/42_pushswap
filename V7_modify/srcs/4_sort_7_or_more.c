af5867ad1ae8fae6e990ed189e79bb14a5cce08c
Yoshifumi-Kitagawa-2 <kitapo3110@gmail.com>
2023-01-25 14:11:09 +0900

    commit

--- V7_modify/srcs/4_sort_7_or_more.c
+++ V7_modify/srcs/4_sort_7_or_more.c
@@ -6,7 +6,7 @@
 /*   By: yokitaga <yokitaga@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/23 16:50:45 by yokitaga          #+#    #+#             */
-/*   Updated: 2023/01/25 14:10:48 by yokitaga         ###   ########.fr       */
+/*   Updated: 2023/01/25 14:11:02 by yokitaga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -130,7 +130,7 @@ void    quick_sort(t_data *data, size_t index_1)
     stack_a_len = ft_stack_size(data->stack_a);
     i =0;
     current = data->stack_a;
-    while (i <stack_a_len)
+    while (i < stack_a_len)
     {
         if (current->sorted_index >= index_1)
             operation_pb(data);
